5d1784eaeffb8ebf20c675ffcc27340b6e0dd0eb
Kurt Peek <kurt.peek@gmail.com>
2019-05-04 09:19:05 -0700

    Use dotenv

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './App.css';
-
 import { MapWithGroundOverlay } from "./components/MapWithGroundOverlay"
+
+require('dotenv').config()
 
 function App() {
   return (
     <div className="App">
       <MapWithGroundOverlay
-        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCZOrQwopojhrGf9A1NHnPmBpZf6rWpoK8&v=3.exp&libraries=geometry,drawing,places"
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&v=3.exp&libraries=geometry,drawing,places`}
         loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `400px` }} />}
+        containerElement={<div style={{ height: `600px` }} />}
         mapElement={<div style={{ height: `100%` }} />}
       />
     </div>
